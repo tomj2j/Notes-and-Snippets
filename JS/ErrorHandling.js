e14af94f9e5a41e900edabfe99eab0d8ec1cfa3e
Thomas Werner <tom.renrew@gmail.com>
2022-07-16 15:25:15 +0200

    Create ErrorHandling.js

---
+++ JS/ErrorHandling.js
@@ -0,0 +1,39 @@
+// Error Handling
+
+function addTwoNums(a, b) {
+  try {
+    if (typeof a != "number") {
+      throw new ReferenceError("the first argument is not a number");
+    } else if (typeof b != "number") {
+      throw new ReferenceError("the second argument is not a number");
+    } else {
+      console.log(a + b);
+    }
+  } catch (err) {
+    console.log("Error!", err);
+  }
+}
+addTwoNums(5, "5");
+console.log("It still works");
+// Error handling prevented the script to stop
+
+//
+
+function letterFinder(word, match) {
+  var condition1 = typeof word == "string" && word.length >= 2;
+  var condition2 = typeof match == "string" && match.length == 1;
+  if (condition1 && condition2) {
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] == match) {
+        console.log("Found the", match, "at", i);
+      } else {
+        console.log("---No match found at", i);
+      }
+    }
+  } else {
+    console.log("Please pass correct arguments to the function");
+  }
+}
+//letterFinder([],[])
+letterFinder("cat", "c");
+letterFinder("thomas", "o");
